b649219fcdeaddb5e2cb7b4293de3d27909c6a2e
BlueShork <mrcalcifer123@gmail.com>
2021-03-14 19:20:19 +0100

    Modification for patch a view bug

--- script/script.js
+++ script/script.js
@@ -17,19 +17,21 @@ for(var i = 0;  i < element.length; i++){
             scoreboard.classList.add('score');
             scoreboard.classList.remove('remove_display');
         })
-
+        scoreboard.addEventListener('mouseout', function(){
+            scoreboard.classList.add('remove_display');
+        })
     })
     p.addEventListener('mouseout', function(){
         this.classList.remove('active');
         scoreboard.classList.add('remove_display');
-        
     })
 }
 
 scoreboard.addEventListener('mouseout', function(){
     scoreboard.classList.add('remove_display');
-    
+   
 })
+
 
 
 
